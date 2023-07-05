51cde8ff3906971262b3c37f4831cdf280cba4a6
Niklas Wolf <wolfniklas94@web.de>
2023-07-05 21:17:54 +0200

    Init Nuxt & Tauri

---
+++ app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { invoke } from '@tauri-apps/api'
+
+// now we can call our Command!
+// Right-click the application background and open the developer tools.
+// You will see "Hello, World!" printed in the console!
+invoke('greet', { name: 'World' })
+    // `invoke` returns a Promise
+    .then((response) => console.log(response))
+</script>
+<template>
+  <div>
+    <NuxtWelcome/>
+  </div>
+</template>
